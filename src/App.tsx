4877fbb0c776bcadc225c33506c7cc1b662b561f
Estheixys Peña <pehinojosa72@gmail.com>
2021-03-15 16:12:29 -0400

    React + TypeScript: useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Counter } from "./components/Counter";
+import { Usuario } from "./components/Usuario";
 
 function App() {
   return (
     <>
       <h1> React + TypeScript </h1>
+      <hr/>
+
+      <Counter />
+
+      <Usuario />
     </>
   );
 }
